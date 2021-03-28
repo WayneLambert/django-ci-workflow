3c6aaa5d7f667ace8e1767ed86753eb42bb01c25
Wayne Lambert <wayne.a.lambert@gmail.com>
2021-03-28 15:22:03 +0100

    Amend `manage.py`
    Addresses the following:
    
    - Adds `rich` tracebacks
    - Adds debug config to `runserver` startup

--- manage.py
+++ manage.py
@@ -2,19 +2,28 @@
 import os
 import sys
 
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ['DJANGO_SETTINGS_MODULE'])
     try:
+        from django.conf import settings
         from django.core.management import execute_from_command_line
-    except ImportError:
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+
+        if settings.DEBUG:
+            from rich import pretty, traceback
+            pretty.install()
+            traceback.install()
+
+            if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+                import ptvsd
+                ptvsd.enable_attach(address=('0.0.0.0', 8890))
+                print("Attached remote debugger to Docker container")
+
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
     execute_from_command_line(sys.argv)
